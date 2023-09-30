e483d12549aa9fa213e6c8727683c2e2cd18fcd6
Ningaanza Remy <remimaradona@gmail.com>
2023-09-30 14:57:10 -0700

    react reCAPTCHA implementation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,23 @@
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {ReCAPTCHA} from 'react-google-recaptcha';
 
 const Contact = () => {
   const form = useRef();
-
+  const recaptchaRef = useRef();
+  const [isRecaptchaVerified, setIsRecaptchaVerified] = useState(false);
   const sendEmail = e => {
     e.preventDefault();
     emailjs
-      .sendForm(
+    .sendForm(
         process.env.REACT_APP_EMAILJS_SERVICE_ID.toString(),
         process.env.REACT_APP_EMAILJS_TEMPLATE_ID.toString(),
         form.current,
         process.env.REACT_APP_EMAILJS_USER_ID.toString(),
-      )
-      .then(
+    )
+    .then(
         result => {
           toast.success('Message Sent Successfully!', {
             position: 'top-right',
@@ -27,6 +29,9 @@ const Contact = () => {
             progress: undefined,
           });
           document.getElementById('myForm').reset();
+          // Reset reCAPTCHA after successful email submission
+          recaptchaRef.current.reset();
+          setIsRecaptchaVerified(false);
         },
         error => {
           toast.error('Ops Message Not Sent!', {
@@ -39,66 +44,80 @@ const Contact = () => {
             progress: undefined,
           });
         },
-      );
+    );
   };
-
   return (
-    <>
-      <form id="myForm" className="contactform" ref={form} onSubmit={sendEmail}>
-        <div className="row">
-          <div className="col-12 col-md-6">
-            <div className="form-group">
-              <input type="text" name="name" placeholder="YOUR NAME" required />
+      <>
+        <form id="myForm" className="contactform" ref={form}
+              onSubmit={sendEmail}>
+          <div className="row">
+            <div className="col-12 col-md-6">
+              <div className="form-group">
+                <input type="text" name="name" placeholder="YOUR NAME"
+                       required/>
+              </div>
             </div>
-          </div>
-          {/* End .col */}
+            {/* End .col */}
 
-          <div className="col-12 col-md-6">
-            <div className="form-group">
-              <input
-                type="email"
-                name="user_email"
-                placeholder="YOUR EMAIL"
-                required
-              />
+            <div className="col-12 col-md-6">
+              <div className="form-group">
+                <input
+                    type="email"
+                    name="user_email"
+                    placeholder="YOUR EMAIL"
+                    required
+                />
+              </div>
             </div>
-          </div>
-          {/* End .col */}
+            {/* End .col */}
 
-          <div className="col-12 col-md-12">
-            <div className="form-group">
-              <input
-                type="text"
-                name="subject"
-                placeholder="YOUR SUBJECT"
-                required
-              />
+            <div className="col-12 col-md-12">
+              <div className="form-group">
+                <input
+                    type="text"
+                    name="subject"
+                    placeholder="YOUR SUBJECT"
+                    required
+                />
+              </div>
             </div>
-          </div>
-          {/* End .col */}
+            {/* End .col */}
 
-          <div className="col-12">
-            <div className="form-group">
+            <div className="col-12">
+              <div className="form-group">
               <textarea
-                name="message"
-                placeholder="YOUR MESSAGE"
-                required
+                  name="message"
+                  placeholder="YOUR MESSAGE"
+                  required
               ></textarea>
+              </div>
             </div>
-          </div>
-          {/* End .col */}
+            {/* End .col */}
 
-          <div className="col-12">
-            <button type="submit" className="button">
-              <span className="button-text">Send Message</span>
-              <span className="button-icon fa fa-send"></span>
-            </button>
+            <div className="col-12">
+              <button type="submit" className="button"
+                      disabled={!isRecaptchaVerified}>
+                <span className="button-text">Send Message</span>
+                <span className="button-icon fa fa-send"></span>
+              </button>
+            </div>
+            <div className="col-12">
+              <ReCAPTCHA
+                  ref={recaptchaRef}
+                  sitekey={process.env.REACT_APP_SITE_KEY.toString()}
+                  onChange={() => setIsRecaptchaVerified(true)}
+                  onExpired={() => setIsRecaptchaVerified(false)}
+                  onErrored={() => {
+                    toast.error(
+                        'Error with reCAPTCHA. Please try again later.');
+                    setIsRecaptchaVerified(false);
+                  }}
+              />
+            </div>
+            {/* End .col */}
           </div>
-          {/* End .col */}
-        </div>
-      </form>
-    </>
+        </form>
+      </>
   );
 };
-
 export default Contact;
